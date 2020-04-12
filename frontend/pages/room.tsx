596caac8c9a3ae145c2cf087f87d4eb4b7ae8d1b
Yujiro Yahata <syani4732@gmail.com>
2020-04-12 13:26:04 +0900

    複数人通話機能一応完成(メッセージ送信周りが逆にうまく行っていない)

--- frontend/pages/room.tsx
+++ frontend/pages/room.tsx
@@ -4,6 +4,9 @@ import Peer, { RoomStream } from "skyway-js";
 import { API_PATH, SKYWAY_API_KEY } from "./env";
 
 const Room = (props) => {
+  const jsRemoteStream = document.getElementById("js-remote-streams");
+  const jsSendTrigger = document.getElementById("js-send-trigger");
+  const jsLeaveTrigger = document.getElementById("js-leave-trigger");
   const getRoomModeByHash = () => (location.hash === "#sfu" ? "sfu" : "mesh");
 
   const localStreamRef = useRef<HTMLVideoElement>(null);
@@ -30,14 +33,7 @@ const Room = (props) => {
   const [roomMode, setRoomMode] = useState(getRoomModeByHash());
   const [roomId, setRoomId] = useState("");
   const [roomMessages, setRoomMessages] = useState("");
-
-  const [remoteStreams, setRemoteStreams] = useState<RoomStream[]>([]);
-
   const [localText, setLocalText] = useState("");
-
-  //const [localVideoMuted, setLocalVideoMuted] = useState(true);
-  //const [localVideoPlaysInline, setLocalVideoPlaysInline] = useState(true);
-
   const [peer, setPeer] = useState(new Peer({ key: SKYWAY_API_KEY }));
 
   window.addEventListener("hashchange", () => setRoomMode(getRoomModeByHash()));
@@ -65,12 +61,17 @@ const Room = (props) => {
     room.once("open", () => {
       setRoomMessages(roomMessages + "=== You joined ===\n");
     });
-    room.on("peerJoin", (peerId) => {
+    room.on("peerJoin", (peerId: string) => {
       setRoomMessages(roomMessages + `=== ${peerId} joined ===\n`);
     });
 
-    room.on("stream", async (stream: RoomStream) => {
-      setRemoteStreams([...remoteStreams, stream]);
+    room.on("stream", async (stream) => {
+      const newVideo = document.createElement("video");
+      newVideo.srcObject = stream;
+      // newVideo.playsInline = true;
+      newVideo.setAttribute("data-peer-id", stream.peerId);
+      jsRemoteStream.append(newVideo);
+      await newVideo.play().catch(console.error);
     });
 
     room.on("data", ({ data, src }) => {
@@ -80,38 +81,37 @@ const Room = (props) => {
 
     // for closing room members
     room.on("peerLeave", (peerId: string) => {
-      setRemoteStreams(
-        remoteStreams.filter((remoteStream) => {
-          if (peerId === remoteStream.peerId) {
-            remoteStream.getTracks().forEach((track) => track.stop());
-            remoteStream = null;
-          }
-          return peerId != remoteStream.peerId;
-        })
+      const remoteVideo: HTMLVideoElement = jsRemoteStream.querySelector(
+        `[data-peer-id=${peerId}]`
       );
 
+      if (remoteVideo.srcObject instanceof MediaStream) {
+        remoteVideo.srcObject.getTracks().forEach((track) => track.stop());
+      }
+      remoteVideo.srcObject = null;
+      remoteVideo.remove();
       setRoomMessages(roomMessages + `=== ${peerId} left ===\n`);
     });
 
     // for closing myself
     room.once("close", () => {
-      document
-        .getElementById("js-send-trigger")
-        .removeEventListener("click", onClickSend);
+      jsSendTrigger.removeEventListener("click", onClickSend);
       setRoomMessages(roomMessages + "== You left ===\n");
-      remoteStreams.map((remoteStream) => {
-        remoteStream.getTracks().forEach((track) => track.stop());
-        remoteStream = null;
-      });
-      setRemoteStreams([]);
+      Array.from(jsRemoteStream.children).forEach(
+        (remoteVideo: HTMLVideoElement) => {
+          if (remoteVideo.srcObject instanceof MediaStream) {
+            remoteVideo.srcObject.getTracks().forEach((track) => track.stop());
+          }
+          remoteVideo.srcObject = null;
+          remoteVideo.remove();
+        }
+      );
     });
 
-    document
-      .getElementById("js-send-trigger")
-      .addEventListener("click", onClickSend);
-    document
-      .getElementById("js-leave-trigger")
-      .addEventListener("click", () => room.close(), { once: true });
+    jsSendTrigger.addEventListener("click", onClickSend);
+    jsLeaveTrigger.addEventListener("click", () => room.close(), {
+      once: true,
+    });
 
     function onClickSend() {
       // Send message to all of the peers in the room via websocket
@@ -156,29 +156,19 @@ const Room = (props) => {
             <button id="js-leave-trigger">Leave</button>
           </div>
 
-          <div className="remote-streams" id="js-remote-streams">
-            {remoteStreams.map((remoteStream: RoomStream) => {
-              return (
-                <video
-                  ref={async (video) => {
-                    // FIXME: リモートストリームが切れた(誰かが退出し)とき, remoteStreamがnullになって型エラーで落ちる
-                    video.srcObject = remoteStream;
-                    await video.play();
-                  }}
-                  playsInline
-                  id={remoteStream.peerId}
-                ></video>
-              );
-            })}
-          </div>
+          <div className="remote-streams" id="js-remote-streams"></div>
 
           <div>
-            <pre className="messages" id="js-messages"></pre>
+            <pre className="messages" id="js-messages">
+              {roomMessages}
+            </pre>
             <input
               type="text"
               id="js-local-text"
               value={localText}
-              onChange={(e) => setLocalText(e.target.value)}
+              onChange={(e) => {
+                setLocalText(e.target.value);
+              }}
             />
             <button id="js-send-trigger">Send</button>
           </div>
