4484cdbfcb69961df91dce5efd545baa2f670a84
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-05-10 13:32:30 +0900

    WIP roomsの実装

--- frontend/database/index.ts
+++ frontend/database/index.ts
@@ -5,3 +5,8 @@ export const categories = async () => {
     const db = firebase.firestore();
     return db.collection('categories');
 }
+
+export const rooms = async (cid: number) => {
+    const db = firebase.firestore();
+    return db.collection('categories').doc(`${cid}`).get();
+}
