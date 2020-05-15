b45da855b9a5c47bd3c61bd1c5db00e844557670
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-05-15 11:43:06 +0900

    DB機能追加

--- frontend/database/index.ts
+++ frontend/database/index.ts
@@ -1,4 +1,5 @@
 import firebase from './../plugins/firebase'
+import {Room, User} from "./model";
 
 
 export const categories = async () => {
@@ -9,4 +10,23 @@ export const categories = async () => {
 export const rooms = async (cid: number) => {
     const db = firebase.firestore();
     return db.collection('categories').doc(`${cid}`).collection('rooms');
+}
+
+export const createRoom = async (cid: number,room: Room) => {
+    const db = firebase.firestore();
+    await db.collection('categories').doc(`${cid}`).collection('rooms').add(room);
+}
+
+export const joinRoom = async (cid: number,docId: string,user: User) => {
+    const db = firebase.firestore();
+    await db.collection('categories').doc(`${cid}`).collection('rooms').doc(docId).update({
+        users: firebase.firestore.FieldValue.arrayUnion(user)
+    });
+}
+
+export const leaveRoom = async (cid: number,docId: string,user: User) => {
+    const db = firebase.firestore();
+    await db.collection('categories').doc(`${cid}`).collection('rooms').doc(docId).update({
+        users: firebase.firestore.FieldValue.arrayRemove(user)
+    });
 }
