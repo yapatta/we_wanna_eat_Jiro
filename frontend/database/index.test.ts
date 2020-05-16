9110536650f63f8f4e7897f3a9e7f1344511d5ae
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-05-16 16:33:07 +0900

    命名修正

--- frontend/database/index.test.ts
+++ frontend/database/index.test.ts
@@ -1,14 +1,18 @@
-import {categories, createRoom, joinRoom, leaveRoom, rooms} from "./index";
+import {
+    selectCategories,
+    selectRoomDocument,
+    insertRoomDocument, updateRoomDocumentWhenLeaved, updateRoomDocumentWhenJoined
+} from "./index";
 import DocumentData = firebase.firestore.DocumentData;
-import {Room} from "./model";
+import {RoomDocument} from "./model";
 
 /**
  * 動作確認用のテストメソッドです。これらはdbとの疎通テスト用に使われます。
  * https://console.firebase.google.com/u/0/project/we-wanna-eat-jiro/database/firestore/
  */
-describe('categories が動作すること。', () => {
+describe('selectCategories が動作すること。', () => {
     test('データ数テスト', async () => {
-        const c = await categories();
+        const c = await selectCategories();
         const docs = await c.get();
 
         docs.forEach(doc => {
@@ -20,32 +24,43 @@ describe('categories が動作すること。', () => {
 
 })
 
-describe('roomの取得メソッドrooms が動作すること。', () => {
+describe('RoomDocumentの取得メソッドselectRoomDocument が動作すること。', () => {
     test('データ数テスト', async () => {
-        const rs = await (await rooms(2)).get();
+        const rs = await (await selectRoomDocument(2)).get();
         rs.forEach(el => {
             console.log(el.id,'=>',el.data())
         })
-        expect(rs.docs.length).toEqual(2);
+        expect(rs.docs.length).toEqual(1);
     })
 })
 
-describe('roomの作成メソッド createRoom が動作すること。', () => {
+describe('RoomDocumentの作成メソッド insertRoomDocument  が動作すること。', () => {
     test('投稿テスト', async () => {
-        const room:Room = {
-            name : "test_room",
+        const roomDocument:RoomDocument = {
+            name : "test_RoomDocument",
             admin: "test_admin",
             admin_uid: "test_uid",
             description: "test_description",
             users: []
         }
-        await createRoom(2,room);
+        await insertRoomDocument(1,roomDocument);
     })
 })
 
-describe('leaveRoom 人間の退出メソッド', () => {
+describe('人間の侵入メソッド updateRoomDocumentWhenJoinedが動作すること。', () => {
     test('退出テスト', async () => {
-        await leaveRoom(2,"uw7D5CSkHs8qcyRMncMw",{
+        await updateRoomDocumentWhenJoined(1,"aWF0r7FaOvMEh4RN3SVL",{
+            uid: "test uid",
+            nickname: "test nickname",
+            introduction: "Howdy!",
+            evaluation: 3
+        })
+    })
+})
+
+describe('人間の退出メソッド updateRoomDocumentWhenLeavedが動作すること。', () => {
+    test('退出テスト', async () => {
+        await updateRoomDocumentWhenLeaved(1,"aWF0r7FaOvMEh4RN3SVL",{
             uid: "test uid",
             nickname: "test nickname",
             introduction: "Howdy!",
