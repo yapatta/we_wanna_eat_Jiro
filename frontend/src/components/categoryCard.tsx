052f25481e28219bde3c1e09c9c14fc023619a7d
yuziroppe <syani4732@gmail.com>
2020-05-21 22:27:48 +0900

    デザインとrouteの微妙な修正

--- frontend/src/components/categoryCard.tsx
+++ frontend/src/components/categoryCard.tsx
@@ -1,7 +1,6 @@
-import { CardContent, Typography } from '@material-ui/core';
+import { CardContent, Typography, CardActionArea } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
-import { Grid } from '@material-ui/core';
-
+import { Grid, makeStyles } from '@material-ui/core';
 import Link from 'next/link';
 import React from 'react';
 import { CategoryDocument } from '../database/model';
@@ -10,29 +9,38 @@ type CategoryCardProps = {
   children: CategoryDocument;
 };
 
+const useStyles = makeStyles((theme) => ({
+  category: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const CategoryCard = (props: CategoryCardProps) => {
+  const classes = useStyles();
   return (
     <Grid item xs={12} md={6} lg={4}>
-      <Card>
-        <CardContent>
-          <Typography component="h2">
-            <Link
-              href={{
-                pathname: '/categories',
-                query: { index: `${props.children.cid}` },
-              }}
-              as={`/${props.children.name}`}
-            >
-              <div>
-                {
-                  // TODO: (@reud) 良い感じのカードレイアウトにする
-                }
-                <h3>{props.children.name}</h3>
-                <p>{props.children.description}</p>
-              </div>
-            </Link>
-          </Typography>
-        </CardContent>
+      <Card className={classes.category}>
+        <CardActionArea>
+          <CardContent>
+            <Typography gutterBottom component="h2">
+              <Link
+                href={{
+                  pathname: '/categories/[id]',
+                  query: { index: `${props.children.cid}` },
+                }}
+                as={`/categories/${props.children.cid}`}
+              >
+                <div>
+                  {
+                    // TODO: (@reud) 良い感じのカードレイアウトにする
+                  }
+                  <h3>{props.children.name}</h3>
+                  <p>{props.children.description}</p>
+                </div>
+              </Link>
+            </Typography>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </Grid>
   );
---
+++ frontend/src/pages/categories/[cid].tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+import RoomCard from '../../components/roomCard';
+import { Grid, Container } from '@material-ui/core';
+import { RoomDocument } from '../../database/model';
+import { selectRoomDocument } from '../../database';
+
+interface CategoryProps {
+  rooms: RoomDocument[];
+}
+
+const Categories = (props: CategoryProps) => {
+  return (
+    <Layout>
+      <Container maxWidth="lg">
+        <Grid container>
+          {props.rooms.map((room: RoomDocument, index: number) => {
+            return <RoomCard key={index}>{room}</RoomCard>;
+          })}
+        </Grid>
+        <Link href="/">
+          <a>ホームに戻る</a>
+        </Link>
+      </Container>
+    </Layout>
+  );
+};
+
+Categories.getInitialProps = async (props) => {
+  const res = await selectRoomDocument(Number(props.query.cid));
+  const rooms: RoomDocument[] = [];
+  const docs = await res.get();
+  docs.forEach((doc) => rooms.push(doc.data() as RoomDocument));
+  return { rooms };
+};
+export default Categories;
