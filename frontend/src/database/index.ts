fb84205ad7a3f11945e937d985aa29c684602f23
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2020-05-21 12:55:13 +0900

    メソッドの移動

--- frontend/src/database/index.ts
+++ frontend/src/database/index.ts
@@ -65,6 +65,18 @@ export const updateRoomDocumentWhenLeaved = async (
     });
 };
 
+export const createUser = async (userDoc: UserDocument) => {
+  const db = firebase.firestore();
+  const docRef = db.collection('users').doc(userDoc.uid);
+  await docRef.set(userDoc);
+};
+
+export const isCreatedUser = async (uid: string) => {
+  const db = firebase.firestore();
+  const user = await db.collection('users').doc(uid).get();
+  return user.exists;
+};
+
 export const _insertCategoryDocument = async (
   docId: number,
   categoryDocument: CategoryDocument,
