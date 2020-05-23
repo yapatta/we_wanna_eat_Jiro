65ebf9df430cd48ef58a04e54bfb87686db1c29b
Piece123 <taka.rock.i4@gmail.com>
2020-05-23 17:32:03 +0900

    テーマカラーを変更

--- frontend/src/components/theme.tsx
+++ frontend/src/components/theme.tsx
@@ -4,16 +4,16 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#ffd180',
     },
     secondary: {
-      main: '#19857b',
+      main: '#ffffff',
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: '#fafafa',
     },
   },
 });
